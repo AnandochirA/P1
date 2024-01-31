1d793737db976b5546dc728eb3d0241a9173d2b4
AnandochirA <158313510+AnandochirA@users.noreply.github.com>
2024-01-31 11:36:33 -0500

    Add files via upload

---
+++ Die.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class Die
+{
+    private Random rand;
+    private int[] outcomes;
+
+    // Constructor for a standard six-sided die
+    public Die(int seed)
+    {
+        outcomes = new int[]{1, 2, 3, 4, 5, 6};
+        rand = new Random(seed);
+    }
+
+    public Die(int seed, int[] GivenOutcomes)
+    {
+        outcomes = new int[GivenOutcomes.length];
+        for (int i = 0; i < GivenOutcomes.length; i++) {
+            outcomes[i] = GivenOutcomes[i];
+        }
+        rand = new Random(seed);
+    }
+
+    public int nextRoll()
+    {
+        int roll = rand.nextInt(outcomes.length);
+        return outcomes[roll];
+    }
+}
