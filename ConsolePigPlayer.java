a9ad6bbb26d423b67e96eca13bcefdfca1d0eb68
AnandochirA <158313510+AnandochirA@users.noreply.github.com>
2024-02-08 22:10:06 -0500

    Update ConsolePigPlayer.java

--- ConsolePigPlayer.java
+++ ConsolePigPlayer.java
@@ -1,49 +1,20 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
-public interface PigUI
-{
-    int	askUserForSeed();
-    void displayCurrentScores​(int player1Score, int player2Score);
-    void displayGameOver​(int player1Score, int player2Score);
-    void displayPlayersTurn​(int playerNumber);
-    void displayRollResult​(int diceRoll);
-    void displayTurnResults​(int playerNumber, int turnTotal, int newScore);
-}
+public class ConsolePigPlayer implements PigPlayer {
+    private Scanner scanner;
+    private PrintStream out;
 
-public class ConsoleInterface implements PigUI
-{
-    public ConsoleInterface ​(java.util.Scanner scanner, java.io.PrintStream out) {}
-    public int askUserForSeed()
+    //constructor of the class
+    public ConsolePigPlayer(Scanner scanner, PrintStream out) 
     {
-        System.out.println("What seed would you like?");
-        Scanner myObj = new Scanner(System.in);
-        myObj.close();
-        int x = myObj.nextInt();
-        return x;
+        this.scanner = scanner;
+        this.out = out;
     }
-    public void displayCurrentScores​(int player1Score, int player2Score)
-    {
-        System.out.println("Player 1 score:" + player1Score);
-        System.out.println("Player 2 score:" + player2Score);
-    }
-    public void displayGameOver​(int player1Score, int player2Score)
-    {
-        System.out.println("Game Over!!!");
-        System.out.println("Player 1 score:" + player1Score);
-        System.out.println("Player 2 score:" + player2Score);
-    }
-    public void displayPlayersTurn​(int playerNumber)
-    {
-        System.out.println("It is player " + playerNumber + "'s turn");
-    }
-    public void displayRollResult​(int diceRoll)
-    {
-        System.out.println("Roll:" );
-        //How to dice roll???
-    }
-    public void displayTurnResults​(int playerNumber, int turnTotal, int newScore)
-    {
-        System.out.println("Turn total: " + turnTotal);
-        System.out.println("New Score: " + newScore);
+    //isRolling function which defines the player wants hold it or not 
+    public boolean isRolling(int myScore, int otherScore, int turnTotal) {
+        out.print("Turn total: " + turnTotal + "    " + "Roll/Hold? ");
+        String input = scanner.nextLine(); // Read user input 
+        return input.isEmpty(); // If input is empty, return true (roll again), otherwise return false (hold)
     }
 }
