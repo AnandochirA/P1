0c3d53218f66cdfe65cad1defe84c11ae7f3af57
AnandochirA <158313510+AnandochirA@users.noreply.github.com>
2024-02-13 20:43:42 -0500

    Update ConsolePigPlayer.java

--- ConsolePigPlayer.java
+++ ConsolePigPlayer.java
@@ -1,20 +1,22 @@
+package pig;
+
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class ConsolePigPlayer implements PigPlayer {
     private Scanner scanner;
     private PrintStream out;
-
-    //constructor of the class
-    public ConsolePigPlayer(Scanner scanner, PrintStream out) 
-    {
+    // Constructor of the class
+    public ConsolePigPlayer(Scanner scanner, PrintStream out) {
         this.scanner = scanner;
         this.out = out;
     }
-    //isRolling function which defines the player wants hold it or not 
+
+    // isRolling function which defines if the player wants to hold or not
     public boolean isRolling(int myScore, int otherScore, int turnTotal) {
-        out.print("Turn total: " + turnTotal + "    " + "Roll/Hold? ");
-        String input = scanner.nextLine(); // Read user input 
-        return input.isEmpty(); // If input is empty, return true (roll again), otherwise return false (hold)
+        System.out.println("Turn total: " + turnTotal + "    " + "Roll/Hold? "); //program need to ask about the user's decision
+        String input = scanner.nextLine(); // Read user input
+        return input.isEmpty() && otherScore < 100 && myScore < 100;
     }
+
 }
