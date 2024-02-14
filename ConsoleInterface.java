0290e4c951c949c2f53b31f3080e4d74b44cbeac
AnandochirA <158313510+AnandochirA@users.noreply.github.com>
2024-02-13 20:43:16 -0500

    Update ConsoleInterface.java

--- ConsoleInterface.java
+++ ConsoleInterface.java
@@ -1,42 +1,42 @@
+package pig;
+
+import java.io.PrintStream;
 import java.util.Scanner;
 
-public class ConsoleInterface implements PigUI 
+public class ConsoleInterface implements PigUI
 {
-    public ConsoleInterface (java.util.Scanner scanner, java.io.PrintStream out) {}
+    public ConsoleInterface (Scanner scanner, PrintStream out) {}
 
-    public int askUserForSeed() 
-    {
+    public int askUserForSeed(Scanner scanner) {
         System.out.println("What seed would you like?");
-        Scanner myObj = new Scanner(System.in);
-        int x = myObj.nextInt();
-        myObj.close();
+        int x = scanner.nextInt();
         return x;
     }
 
-    public void displayCurrentScore(int player1Score, int player2Score) 
+    public void displayCurrentScores(int player1Score, int player2Score)
     {
         System.out.println("Player 1 score: " + player1Score);
         System.out.println("Player 2 score: " + player2Score);
     }
 
-    public void displayGameOver(int player1Score, int player2Score) 
+    public void displayGameOver(int player1Score, int player2Score)
     {
         System.out.println("Game Over!!!");
         System.out.println("Player 1 score: " + player1Score);
         System.out.println("Player 2 score: " + player2Score);
     }
 
-    public void displayPlayersTurn(int playerNumber) 
+    public void displayPlayersTurn(int playerNumber)
     {
         System.out.println("It is player " + playerNumber + "'s turn");
     }
 
-    public void displayRollResult(int diceRoll) 
+    public void displayRollResult(int diceRoll)
     {
         System.out.println("Roll: " + diceRoll);
     }
 
-    public void displayTurnResults(int playerNumber, int turnTotal, int newScore) 
+    public void displayTurnResults(int playerNumber, int turnTotal, int newScore)
     {
         System.out.println("Turn total: " + turnTotal);
         System.out.println("New Score: " + newScore);
