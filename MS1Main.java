971d0f520c012f34ad20c56192ba41fae7221fc0
AnandochirA <158313510+AnandochirA@users.noreply.github.com>
2024-02-13 20:44:34 -0500

    Update MS1Main.java

--- MS1Main.java
+++ MS1Main.java
@@ -1,17 +1,17 @@
+package pig;
+
 import java.util.Scanner;
 
 public class MS1Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Please enter the seed:"); // Asking seed and reading it
-        Scanner s = new Scanner(System.in);
-        int seed = s.nextInt();
-        Die MyDice = new Die(seed);
+        Die MyDice = new Die(87);
 
         for (int i = 0; i < 10; i++) // Rolling 10 times
         {
             int result = MyDice.nextRoll();
             System.out.println(result);
         }
-        s.close();
     }
 }
