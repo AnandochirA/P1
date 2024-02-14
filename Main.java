876d97ae3d2cdbf369b9b58ebdca74025d32c6ba
AnandochirA <158313510+AnandochirA@users.noreply.github.com>
2024-02-13 20:44:57 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,47 @@
+package test;
+
+import pig.*;
+
 import java.util.Scanner;
+
 import static java.lang.System.in;
 import static java.lang.System.out;
 
-public abstract class Main
-{
+/**
+ * This class just tests whether your code will compile with my tests.
+ * This class DOES NOT actually test your code.  It just ensures that it compiles.
+ * Do not change this code.
+ * @author Joe Meehean
+ */
+public class Main {
     public static void main(String[] args)
     {
-        // Create a ConsoleInterface
-        PigUI ui = new ConsoleInterface(new Scanner(in), out);
+        // ConsoleInterface constructor
+        Scanner scan = new Scanner(System.in);
+        PigUI ui = new ConsoleInterface(scan, out);
 
-        // Create seed
-        int seed = ui.askUserForSeed();
+        // PigUI required methods
+        int seed = ui.askUserForSeed(scan);
+        ui.displayRollResult(4);
+        ui.displayCurrentScores(0,1);
+        ui.displayPlayersTurn(1);
+        ui.displayTurnResults(2, 5, 78);
+        ui.displayGameOver(101, 75);
 
-        // Create two pig players
-        PigPlayer player1 = new PigPlayer()
-        {
-            public boolean isRolling(int myScore, int otherScore, int turnTotal) {return true;}
-        };
+        // make a Die
+        Die die = new Die(87);
 
+        // player 1 is not smart
+        // ensures interface isn't broken
+        PigPlayer player1 = new ConsolePigPlayer(scan, out);
+
+        // player 2 is a holdAtXAI
         PigPlayer player2 = new HoldAtXPigPlayer(5);
 
-        // Create a PigGame with the two players
-        PigGame pigGame = new PigGame(player1, player2, seed, ui);
+        // make the game
+        PigGame game = new PigGame(player1, player2, seed, ui);
 
-        // Start the game
-        pigGame.play();
+        // play the game
+        game.play();
     }
 }
